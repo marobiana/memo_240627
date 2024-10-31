af783e86b93708a6d61801a65ab527c13f3e0052
test <test@test.com>
2024-10-31 21:44:51 +0900

    아이디 중복검사 일부

---
+++ src/main/java/com/memo/user/bo/UserBO.java
@@ -0,0 +1,21 @@
+package com.memo.user.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.memo.user.entity.UserEntity;
+import com.memo.user.repository.UserRepository;
+
+@Service
+public class UserBO {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	// 컨트롤러한테
+	// input: loginId
+	// output: UserEntity or null
+	public UserEntity getUserEntityByLoginId(String loginId) {
+		return userRepository.findByLoginId(loginId);
+	}
+}
