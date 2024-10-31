50db482a1689fbe50ff75f16b117b35cf6fec2f2
test <test@test.com>
2024-10-31 20:50:58 +0900

    로그인 페이지 마크업

--- src/main/java/com/memo/user/UserController.java
+++ src/main/java/com/memo/user/UserController.java
@@ -13,4 +13,9 @@ public class UserController {
 		// 가운데 레이아웃 조각만 내려주면 전체 레이아웃과 함께 구성된다.
 		return "user/signUp";
 	}
+	
+	@GetMapping("/sign-in-view")
+	public String signInView() {
+		return "user/signIn";
+	}
 }
