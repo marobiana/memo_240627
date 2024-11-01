02c55ae2776caa9b1182c2653f45f458a48c688a
test <test@test.com>
2024-11-01 19:52:16 +0900

    회원가입 일부

--- src/main/java/com/memo/user/UserRestController.java
+++ src/main/java/com/memo/user/UserRestController.java
@@ -5,10 +5,12 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.memo.common.EncryptUtils;
 import com.memo.user.bo.UserBO;
 import com.memo.user.entity.UserEntity;
 
@@ -18,20 +20,54 @@ public class UserRestController {
 	@Autowired
 	private UserBO userBO;
 
+	/**
+	 * 아이디 중복확인 
+	 * @param loginId
+	 * @return
+	 */
 	@GetMapping("/is-duplicate-id")
 	public Map<String, Object> isDuplicateId(
 			@RequestParam("loginId") String loginId) {
 		
 		// DB select
 		UserEntity user = userBO.getUserEntityByLoginId(loginId);
-		
-		// TODO
+		boolean isDuplicate = false; // 중복 아님
+		if (user != null) {
+			isDuplicate = true;
+		}
 		
 		// 응답값
 		Map<String, Object> result = new HashMap<>();
 		result.put("code", 200);
-		result.put("is_duplicate_id", false);
+		result.put("is_duplicate_id", isDuplicate);
+		return result;
+	}
+	
+	@PostMapping("/sign-up")
+	public Map<String, Object> signUp(
+			@RequestParam("loginId") String loginId,
+			@RequestParam("password") String password,
+			@RequestParam("name") String name,
+			@RequestParam("email") String email) {
+		
+		// md5 알고리즘 - hashing
+		// aaaa -> 74b8733745420d4d33f80c4663dc5e5
+		// aaaa -> 74b8733745420d4d33f80c4663dc5e5
+		String hashedPassword = EncryptUtils.md5(password);
+		
+		// db insert
+		
+		// 응답값
+		Map<String, Object> result = new HashMap<>();
+		result.put("code", 200);
+		result.put("result", "성공");
 		return result;
 	}
 	
 }
+
+
+
+
+
+
