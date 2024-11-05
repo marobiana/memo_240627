ab19899116186c6a392eca7a2ad459730c90d537
test <test@test.com>
2024-11-05 20:51:56 +0900

    글 저장(이미지 빼고)

--- src/main/java/com/memo/post/bo/PostBO.java
+++ src/main/java/com/memo/post/bo/PostBO.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.memo.post.domain.Post;
 import com.memo.post.mapper.PostMapper;
@@ -16,6 +17,18 @@ public class PostBO {
 	
 	public List<Post> getPostListByUserId(int userId) {
 		return postMapper.selectPostListByUserId(userId);
+	}
+	
+	// input: userId, subject, content, file
+	// output: int(성공한 행 개수)
+	public int addPost(int userId, 
+			String subject, String content, MultipartFile file) {
+		
+		String imagePath = null;
+		
+		// TODO file to imagePath
+		
+		return postMapper.insertPost(userId, subject, content, imagePath);
 	}
 }
 
--- src/main/java/com/memo/post/mapper/PostMapper.java
+++ src/main/java/com/memo/post/mapper/PostMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.memo.post.domain.Post;
 
@@ -15,6 +16,13 @@ public interface PostMapper {
 	
 	// input:int(userId) output: List<Post>
 	public List<Post> selectPostListByUserId(int userId);
+	
+	// input:params    output:int or void
+	public int insertPost(
+			@Param("userId") int userId,
+			@Param("subject") String subject,
+			@Param("content") String content,
+			@Param("imagePath") String imagePath);
 }
 
 
